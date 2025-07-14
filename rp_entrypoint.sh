4973d5cfa8f7bd0972adb504fc7be06f1ea429e1
Ni Tianzhen <nitianzhen1210@hotmail.com>
2025-07-14 22:16:32 +0800

    chore: update

--- rp_entrypoint.sh
+++ rp_entrypoint.sh
@@ -4,4 +4,5 @@ cd /runpod-volume/MV-Adapter || exit 1
 git pull
 # apt update
 # apt install -y libegl1-mesa-dev libgl1-mesa-dev
-PATH=/runpod-volume/MV-Adapter/venv/bin:$PATH TORCH_HOME=/runpod-volume/MV-Adapter/.cache/torch HF_HOME=./hf_cache exec /runpod-volume/MV-Adapter/venv/bin/python -u rp_handler.py
+# PATH=/runpod-volume/MV-Adapter/venv/bin:$PATH TORCH_HOME=/runpod-volume/MV-Adapter/.cache/torch HF_HOME=./hf_cache exec /runpod-volume/MV-Adapter/venv/bin/python -u rp_handler.py
+exec /runpod-volume/MV-Adapter/venv/bin/python -u rp_handler.py
